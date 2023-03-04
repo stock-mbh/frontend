09be9f1bfe1f54367e252dd1386c1d4d06b88272
IHeb <ihebferjani04@gmail.com>
2023-03-04 11:04:05 +0100

    deploying on render

--- src/app/shared/global.ts
+++ src/app/shared/global.ts
@@ -1,6 +1,6 @@
 export const GlobalVariable = Object.freeze({
     IMAGE_BASE_PATH: 'https://stock-mbh-backend.herokuapp.com/uploads/',
-    BASE_PATH: 'https://stock-mbh-backend.herokuapp.com',
+    BASE_PATH: 'https://stock-mbh-backend.onrender.com',
     STATES: ["Neuf", "Bon occasion", "Occasion"],
     MONTHES : ['Janvier', 'Février', 'Mars', 'Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre']
 });
